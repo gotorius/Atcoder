a836b84e1430a9285c863360ba57cf4544a7aa02
gotorius <ryosuke1108g@yahoo.ne.jp>
2024-11-06 10:35:56 +0900

    20241106

--- ABC302/B.cpp
+++ ABC302/B.cpp
@@ -4,8 +4,8 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
-int dx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
-int dy[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int di[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+int dj[8] = {0, 0, 1, -1, 1, -1, 1, -1};
 
 int main() {
     int h, w;
@@ -13,8 +13,25 @@ int main() {
     vector<string> s(h);
     rep(i,h) cin >> s[i];
 
+    string T = "snuke";
+
     rep(si,h)rep(sj,w){
-        
+        rep(v,8){
+            int i = si, j = sj;
+            rep(k,5){
+                if(i<0 || j<0 || i>=h || j>=w) break;
+                if(s[i][j] != T[k]) break;
+                if(k == 4){
+                    i = si, j = sj;
+                    rep(nk,5){
+                        printf("%d %d\n",i+1, j+1);
+                        i += di[v]; j += dj[v];
+                    }
+                    return 0;
+                }
+                i += di[v]; j += dj[v];
+            }
+        }
     }
     return 0;
 }
