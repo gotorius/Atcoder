7ceaccc5a3a499166e0d5d39f2d636b5f6eaff76
gotorius <ryosuke1108g@yahoo.ne.jp>
2024-11-24 23:51:27 +0900

    20241124

---
+++ ABC365/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  
+    int y;
+    cin >> y;
+    if(y%4 != 0 ){
+      cout << 365 << endl;
+    }else if(y%400 != 0 && y%100 == 0){
+      cout << 365 << endl;
+    }else{
+      cout << 366 << endl;
+    }
+    
+    return 0;
+    
+}
+    
