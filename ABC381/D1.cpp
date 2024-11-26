39d93483ffa04245dd568872cc8c2f6eef43dd3a
gotorius <ryosuke1108g@yahoo.ne.jp>
2024-11-26 23:57:02 +0900

    20241126

--- ABC381/D1.cpp
+++ ABC381/D1.cpp
@@ -1,27 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-using ll = long long;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    rep(i,n) cin >> a[i];
-    map<int,int> m;
-    
-    int ans = 0, l = 0, r = 0;
-    while(r < n){
-        l = r;
-        while(r < n && a[r] == a[r+1] && (m.find(a[r]) == m.end())){
-            m[a[r]]++;
-            r += 2;
-            ans = max(ans, r-l);
-        }
-        m.clear();
-        r++;
-    }
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i,n) cin >> a[i];
 
-    cout << ans << endl;
-    return 0;
+  int ans = 0;
+  rep(si,2) {
+    vector<int> cnt(n+1);
+    int r = si;
+    for (int l = si; l+1 < n; l += 2) {
+      while (r+1 < n) {
+        if (a[r] != a[r+1]) break;
+        if (cnt[a[r]]) break;
+        cnt[a[r]]++;
+        r += 2;
+      }
+      ans = max(ans, r-l);
+      if (l == r) r += 2;
+      else cnt[a[l]]--;
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
 }
