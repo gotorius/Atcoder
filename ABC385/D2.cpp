701ce5d56d1099bf45dfebcbbadf50b7554e28ec
gotorius <ryosuke1108g@yahoo.ne.jp>
2024-12-23 09:49:54 +0900

    20241223

--- ABC385/D2.cpp
+++ ABC385/D2.cpp
@@ -24,9 +24,10 @@ int main() {
         if (d == 'U') {
             if (ypos.find(sx) != ypos.end()) {
                 auto it = ypos[sx].lower_bound(sy);
-                while (it != ypos[sx].end() && *it >= sy && *it < sy + c) {
+                while (it != ypos[sx].end() && *it >= sy && *it <= sy + c) {
                     ans++;
                     xpos[*it].erase(sx);
+                    if (xpos[*it].empty()) xpos.erase(*it);
                     it = ypos[sx].erase(it);
                 }
             }
@@ -34,9 +35,10 @@ int main() {
         } else if (d == 'D') {
             if (ypos.find(sx) != ypos.end()) {
                 auto it = ypos[sx].lower_bound(sy - c);
-                while (it != ypos[sx].end() && *it <= sy && *it > sy - c) {
+                while (it != ypos[sx].end() && *it <= sy && *it >= sy - c) {
                     ans++;
                     xpos[*it].erase(sx);
+                    if (xpos[*it].empty()) xpos.erase(*it);
                     it = ypos[sx].erase(it);
                 }
             }
@@ -44,9 +46,10 @@ int main() {
         } else if (d == 'R') {
             if (xpos.find(sy) != xpos.end()) {
                 auto it = xpos[sy].lower_bound(sx);
-                while (it != xpos[sy].end() && *it >= sx && *it < sx + c) {
+                while (it != xpos[sy].end() && *it >= sx && *it <= sx + c) {
                     ans++;
                     ypos[*it].erase(sy);
+                    if (ypos[*it].empty()) ypos.erase(*it);
                     it = xpos[sy].erase(it);
                 }
             }
@@ -54,16 +57,16 @@ int main() {
         } else if (d == 'L') {
             if (xpos.find(sy) != xpos.end()) {
                 auto it = xpos[sy].lower_bound(sx - c);
-                while (it != xpos[sy].end() && *it <= sx && *it > sx - c) {
+                while (it != xpos[sy].end() && *it <= sx && *it >= sx - c) {
                     ans++;
                     ypos[*it].erase(sy);
+                    if (ypos[*it].empty()) ypos.erase(*it);
                     it = xpos[sy].erase(it);
                 }
             }
             sx -= c;
         }
     }
-    if(xpos.find(sy) != xpos.end() && ypos.find(sx) != ypos.end()) ans++;
 
     cout << sx << ' ' << sy << ' ' << ans << endl;
     return 0;
