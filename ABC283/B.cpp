2b6327750e96e9a8828456fdde212c87239dcc0f
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-01-15 15:42:34 +0900

    20250115

---
+++ ABC283/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i,n) cin >> a[i];
+    int q;
+    cin >> q;
+    rep(i,q){
+        int num;
+        cin >> num;
+        if(num == 1){
+            int k, x;
+            cin >> k >> x;
+            a[k-1] = x;
+        }else{
+            int k;
+            cin >> k;
+            cout << a[k-1] << endl;
+        }
+    }
+    return 0;
+   
+}
