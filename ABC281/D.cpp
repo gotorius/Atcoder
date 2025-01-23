4cfae8c1ce5bacbdb635556583bc33d9ed5392c9
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-01-23 18:51:28 +0900

    20250123

--- ABC281/D.cpp
+++ ABC281/D.cpp
@@ -1,14 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for(int i=0; i<(n); i++)
-using ll = long long;
-using P = pair<int,int>;
 
 int main() {
-    int n, k, d;
-    cin >> n >> k >> d;
-    vector<int> a(n);
-    rep(i,n) cin >> a[i];
     
-   
+	int N,K,D;
+	cin>>N>>K>>D;
+	
+	vector<int> a(N);
+	for(int i=0;i<N;i++)cin>>a[i];
+	
+	vector dp(N+1,vector(K+1,vector<long long>(D,-1)));
+	dp[0][0][0] = 0;
+	
+	for(int i=0;i<N;i++){
+		for(int j=0;j<K+1;j++){
+			for(int k=0;k<D;k++){
+				if(dp[i][j][k]==-1)continue;
+				
+				//a_i を選ばない場合の遷移
+				dp[i+1][j][k] = max(dp[i+1][j][k],dp[i][j][k]);
+				
+				//a_i を選ぶ場合の遷移
+				if(j!=K){
+					dp[i+1][j+1][(k+a[i])%D] = max(dp[i+1][j+1][(k+a[i])%D],dp[i][j][k] + a[i]);
+				}
+			}
+		}
+	}
+	
+	cout<<dp[N][K][0]<<endl;
+	
+	return 0;
 }
