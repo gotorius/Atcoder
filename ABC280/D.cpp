071efecd65b294f0e9ce5bbba23d86ef967d0c34
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-01-30 15:14:48 +0900

    20250130

--- ABC280/D.cpp
+++ ABC280/D.cpp
@@ -3,52 +3,42 @@ using namespace std;
 #define rep(i,n) for(int i=0; i<(n); i++)
 using ll = long long;
 
-vector<ll> simple_sieve(ll limit) {
+vector<int> simple_sieve(int limit) {
     vector<bool> is_prime(limit + 1, true);
-    vector<ll> primes;
+    vector<int> primes;
 
-    for (ll i = 2; i * i <= limit; ++i) {
+    for (int i = 2; i * i <= limit; ++i) {
         if (is_prime[i]) {
-            for (ll j = i * i; j <= limit; j += i) {
+            for (int j = i * i; j <= limit; j += i) {
                 is_prime[j] = false;
             }
         }
     }
 
-    for (ll i = 2; i <= limit; ++i) {
+    for (int i = 2; i <= limit; ++i) {
         if (is_prime[i]) {
             primes.push_back(i);
         }
     }
 
     return primes;
-}
-
-ll f(ll x, ll y){
-    int cnt = 0;
-    while(x % y == 0){
-        x /= y;
-        cnt++;
-    }
-    return cnt;    
 };
 
+
 int main() {
-    ll n = 1e7;
+    int n = 20000000;
     ll k;
     cin >> k;
-    vector<ll> primes = simple_sieve(n);
+    vector<int> primes = simple_sieve(n);
 
-    ll ans = -1;
-    ll cnt = 1;
-    for (ll prime : primes) {
+    int ans = -1;
+    for (int prime : primes) {
         if(k % prime == 0){
             ans = prime;
-            ll k1 = k;
-            rep(i,f(k1,prime)) cnt *= prime;
+            while(k % prime == 0) k /= prime;
         }
     }
-    if(cnt == k) cout << ans << endl;
+    if(k == 1) cout << ans << endl;
     else cout << k << endl;
     return 0;
 }
