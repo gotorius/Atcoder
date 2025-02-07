25b32507943c0a9272eded59d7c7c3ce333d91cb
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-02-07 20:44:10 +0900

    20250207

---
+++ ABC274/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    double ans;
+    ans = b / a;
+    printf("%.3f\n", ans);
+    return 0;
+   
+}
