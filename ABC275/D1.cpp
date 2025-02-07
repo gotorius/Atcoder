267c666f531a995b7b47f23b987c551923920be1
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-02-07 18:56:51 +0900

    20250207

---
+++ ABC275/D1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+
+unordered_map<ll,ll> memo;
+ll f(ll x) {
+  if (x == 0) return 1;
+  ll res = f(x/2)+f(x/3);
+  return res;
+}
+
+int main() {
+  ll n;
+  cin >> n;
+  cout << f(n) << endl;
+  return 0;
+}
