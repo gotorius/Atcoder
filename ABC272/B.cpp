c6688e855e91e5258d56e55d0db033d53e21068d
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-02-15 20:48:06 +0900

    20250215

---
+++ ABC272/B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main(void){
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> x(n);
+    rep(i,m){
+        int k;
+        cin >> k;
+        rep(i,k){}
+    }
+
+}
