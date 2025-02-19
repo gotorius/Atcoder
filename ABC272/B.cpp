12cb473311fb339807580cdebb7b3d1b0a55e2fc
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-02-19 13:23:48 +0900

    20250219

--- ABC272/B.cpp
+++ ABC272/B.cpp
@@ -8,11 +8,31 @@ int main(void){
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> x(n);
+    vector<set<int>> a(n);
     rep(i,m){
         int k;
         cin >> k;
-        rep(i,k){}
-    }
+        vector<int> x(k);
+        set<int> b;
+        rep(i,k){
+            cin >> x[i];
+            x[i]--;
+            b.insert(x[i]);
+        }
 
+        rep(i,n){
+            if(b.count(i)){
+                rep(j,k){
+                    a[i].insert(x[j]);
+                }
+            }
+        }
+    }
+    bool ok = true;
+    rep(i,n){
+        if(a[i].size() != n) ok = false;
+    }
+    if(ok) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
 }
