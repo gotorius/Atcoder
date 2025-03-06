360b2bcf6d6176276c6147863ab3325afb9a9821
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-06 18:23:13 +0900

    20250306

---
+++ ABC302/D.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using mint = modint998244353;
+
+int main() {
+
+}
