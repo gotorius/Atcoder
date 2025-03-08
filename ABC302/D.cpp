ea9320d596a52bca676825ffc0aff5dd4d0ed895
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-08 15:48:21 +0900

    20250308

--- ABC302/D.cpp
+++ ABC302/D.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
+using ll = long long;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 using mint = modint998244353;
 
 int main() {
-    int n, m, d;
+    ll n, m, d;
     cin >> n >> m >> d;
-    vector<int> a(n), b(m);
+    vector<ll> a(n), b(m);
     rep(i,n) cin >> a[i];
     rep(i,m) cin >> b[i];
     sort(b.begin(), b.end());
 
-    int ans = 0;
+    ll ans = -1;
     rep(i,n){
-        auto p = lower_bound(b.begin(), b.end(), a[i]);
-        
+        auto p = upper_bound(b.begin(), b.end(), a[i]+d);
+        if(*p == b[0]) continue;
+        else p--;
+        if(abs(a[i])-*p <= d) ans = max(ans, a[i]+*p);
     }
-
+    cout << ans << endl;
+    return 0;
 }
