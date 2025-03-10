533a6be225770bac0cce55e9fade32b66d423c5d
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-10 23:09:58 +0900

    2050310

---
+++ ABC335/B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+using ll = long long;
+
+int main(void){
+  int n;
+  cin >> n;
+  rep(x,n+1){
+    rep(y,n+1){
+      rep(z,n+1){
+        if(x+y+z <= n){
+          cout << x << ' ' << y << ' ' << z << endl;
+        }
+      }
+    }
+  }
+  return 0;
+}
