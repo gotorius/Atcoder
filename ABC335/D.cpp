e4b0e29493c20792b81b9592fe69cec0507ee145
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-10 23:09:17 +0900

    20250310

--- ABC335/D.cpp
+++ ABC335/D.cpp
@@ -5,5 +5,48 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
+    int n;
+    cin >> n;
+    int now = 0;
+    vector<vector<bool>> visited(n, vector<bool>(n,true));
+    vector<vector<char>> s(n, vector<char>(n, ' '));
+    int h = 0, w = 0;
+    while(now < n*n){
+        while(w < n && visited[h][w]){
+            now++;
+            s[h][w] = 'A' + now;
+            visited[h][w] = false;
+            if(w+1 < n && visited[h][w+1]) w++;
+            else break;
+        }
+        while(h < n && visited[h][w]){
+            now++;
+            s[h][w] = 'A' + now;
+            visited[h][w] = false;
+            if(h+1 < n && visited[h+1][w]) h++;
+            else break;
+        }
+        while(w >= 0 && visited[h][w]){
+            now++;
+            s[h][w] = 'A' + now;
+            visited[h][w] = false;
+            if(w-1 >= 0 && visited[h][w-1]) w--;
+            else break;
+        }
+        while(h >= 0 && visited[h][w]){
+            now++;
+            s[h][w] = 'A' + now;
+            visited[h][w] = false;
+            if(h-1 >= 0 && visited[h-1][w]) h--;
+            else break;
+        }
+    }
+    rep(i,n){
+        rep(j,n){
+            cout << s[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
 
 }
