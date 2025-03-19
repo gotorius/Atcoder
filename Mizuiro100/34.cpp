85fcb52e5150afe5f41d2a22ac1cad1af11de19f
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-19 15:28:03 +0900

    20250319

--- Mizuiro100/34.cpp
+++ Mizuiro100/34.cpp
@@ -4,6 +4,18 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
-int main(void){
+//34　ALDS_10_A - フィボナッチ数　超基本。「DP とは何か」を感じることができます。
 
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> memo(n + 1, -1);
+    function<int(int)> f = [&](int x) {
+        if (x == 0 || x == 1) return 1;
+        if (memo[x] != -1) return memo[x];
+        return memo[x] = f(x - 1) + f(x - 2);
+    };
+    cout << f(n) << endl;
+    return 0;
 }
