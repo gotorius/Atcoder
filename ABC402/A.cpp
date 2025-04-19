b3f4cae7d7e6b6b139227fd0a530baecb0326b87
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-04-20 00:37:12 +0900

    20250420

--- ABC402/A.cpp
+++ ABC402/A.cpp
@@ -5,5 +5,18 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
-
+    string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    set<char> st;
+    rep(i,abc.size()){
+        st.insert(abc[i]);
+    }
+    string s;
+    cin >> s;
+    rep(i,s.size()){
+        if(st.count(s[i])){
+            cout << s[i];
+        }
+    }
+    cout << endl;
+    return 0;
 }
--- ABC402/B.cpp
+++ ABC402/B.cpp
@@ -5,5 +5,24 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
-
+    int q;
+    cin >> q;
+    queue<int> que;
+    rep(i,q){
+        int num;
+        cin >> num;
+        if(num == 1){
+            int x;
+            cin >> x;
+            que.push(x);
+        }else if(num == 2){
+            int ans = 0;
+            if(!que.empty()){
+                ans = que.front();
+                que.pop();
+                cout << ans << endl;
+            }
+        }
+    }
+    return 0;
 }
