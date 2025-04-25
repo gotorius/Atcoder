3470c023ecc8dd00692e9c8e8c12c4977c12e006
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-04-25 21:34:00 +0900

    20250425

--- ABC328/D.cpp
+++ ABC328/D.cpp
@@ -5,5 +5,31 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
-
+    string s;
+    cin >> s;
+    stack<char> st;
+    rep(i,s.size()){
+        st.push(s[i]);
+        while(st.size() > 2 && st.top() == 'C'){
+            char x, y, z;
+            x = st.top(); st.pop();
+            y = st.top(); st.pop();
+            z = st.top(); st.pop();
+            if(x == 'C' && y == 'B' && z == 'A') continue;
+            else{
+                st.push(z);
+                st.push(y);
+                st.push(x);
+                break;
+            }
+        }
+    }
+    string ans = "";
+    while(!st.empty()){
+        ans += st.top();
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    cout << ans << endl;
+    return 0;
 }
