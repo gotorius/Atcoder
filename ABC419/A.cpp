a034d9ee8f39bae8af43bee2287d70b3f1f4c8dc
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-08-18 21:08:16 +0900

    20250818

--- ABC419/A.cpp
+++ ABC419/A.cpp
@@ -5,6 +5,12 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
-    
+    string s;
+    cin >> s;
+    if(s == "red") cout << "SSS" << endl;
+    else if(s == "blue") cout << "FFF" << endl;
+    else if(s == "green") cout << "MMM" << endl;
+    else cout << "Unknown" << endl;
+    return 0; 
 
 }
--- ABC419/D1.cpp
+++ ABC419/D1.cpp
@@ -18,7 +18,23 @@ int main(void){
         a[r] -= 1;
     }
 
-    rep(i,n-1) a[i+1] += a[i];
+    rep(i,n-1) a[i+1] += a[i];    # 1. 統合先レポジトリのディレクトリへ移動
+    cd /path/to/target-repo
+    
+    # 2. 統合元レポジトリをリモート追加
+    git remote add repoA https://github.com/ユーザー名/レポジトリA.git
+    git fetch repoA
+    
+    # 3. サブディレクトリに履歴ごと統合
+    git merge repoA/main --allow-unrelated-histories
+    mkdir repoA
+    git mv * repoA/  # 必要に応じてファイルを移動
+    git commit -m "Move repoA files to subdirectory"
+    
+    # 4. 不要なリモートを削除
+    git remote remove repoA
+    
+    # 5. 他のレポジトリも同様に繰り返す
     rep(i,n){
         if(a[i]%2 == 0) cout << s[i];
         else cout << t[i];
